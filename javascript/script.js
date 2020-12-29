3f845c1661d922273b04e6a1b5e2f62115de3bb0
saddamarbaa <saddamarbaas@gmail.com>
2020-12-29 11:35:55 +0700

    finished Challenge 1: Your Age in Days Project

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,17 @@
 // Challenge 1: Your Age in Days Project
 
-function ageInDays(params) {
+function ageInDays(p) {
   const birthYear = prompt("in which year were you born .. dear frind");
   const YourAgeInDays = (2020 - birthYear) * 365;
   const h2 = document.createElement("h2");
   const textAnswer = document.createTextNode(
-    "You are now " + YourAgeInDays + "days"
+    "You Are now " + YourAgeInDays + " days"
   );
   h2.setAttribute("id", "YourAgeInDays");
+  h2.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h2);
+}
+
+function restAge() {
+  document.getElementById("YourAgeInDays").remove();
 }
