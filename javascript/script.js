2ca312e6a3157dc10782dc2ba535ee4b12ce78df
saddamarbaa <saddamarbaas@gmail.com>
2020-12-31 17:09:55 +0700

    Challenge 3:  Rock Paper Scissors Project working on js file

--- javascript/script.js
+++ javascript/script.js
@@ -30,38 +30,94 @@ function GenerateCat() {
   console.log(randomToRpsInt());
 }
 
-// Challenge 3:  Rock Paper Scissors Project
+// Challenge 3:  rock paper Scissors Project
 
 function rpsGame(yourChoice) {
-  let humanChoice, botChoice, result;
-  humanChoice = yourChoice.id;
-  console.log(humanChoice);
-  botChoice = numberToChoice(randomToRpsInt());
-  // console.log(botChoice);
-  result = decideWinner(humanChoice, botChoice);
+  let humanChoice = yourChoice.id;
+  // console.log(humanChoice);
+
+  let botChoice = numberToChoice(randomToRpsInt());
+  console.log("Computer Choice " + botChoice);
+
+  let result = decideWinner(humanChoice, botChoice);
   // console.log(result);
+
+  /* {"message" : "you won", "color" : "green"} */
+  let message = finalMessage(result);
+  // console.log(message);
+
+  rpsfrontEnd(yourChoice.id, botChoice, message);
 }
 
 // Generate random number
+
 function randomToRpsInt() {
   return Math.floor(Math.random() * 3);
 }
 
 function numberToChoice(number) {
-  return ["rock", "paper", "scissors"][number];
+  return ["rock", "paper", "scissor"][number];
 }
 
 function decideWinner(yourChoice, computerChoice) {
-  let rpsDataBase = {
-    rock: { scissors: 1, rockL: 0.5, paper: 0 },
-    paper: { rock: 1, paper: 0.5, scissors: 0 },
-    scissors: { paper: 1, scissors: 0.5, rock: 0 },
+  const rpsDataBase = {
+    rock: { scissor: 1, rock: 0.5, paper: 0 },
+    paper: { rock: 1, paper: 0.5, scissor: 0 },
+    scissor: { paper: 1, scissor: 0.5, rock: 0 },
   };
 
-  console.log(rpsDataBase[computerChoice][yourChoice]);
+  // console.log(rpsDataBase[yourChoice][computerChoice]);
+  // console.log(rpsDataBase[computerChoice][yourChoice]);
+
   let yourScore = rpsDataBase[yourChoice][computerChoice];
   let computerScore = rpsDataBase[computerChoice][yourChoice];
+  return [yourScore, computerScore];
+}
 
-  return [yourScore][computerScore];
-  // console.log(yourChoice);
+function finalMessage(yourScore, computerScore) {
+  if (yourScore === 0) {
+    return { message: "You Lost!", color: "red" };
+  } else if (yourScore === 0.5) {
+    return { message: "You tied! ", color: "yellow" };
+  } else {
+    return { message: "You Won! ", color: "green" };
+  }
+}
+
+function rpsfrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+  let imagesDataBase = {
+    rock: document.getElementById("rock").src,
+    scissor: document.getElementById("scissor").src,
+    paper: document.getElementById("paper").src,
+  };
+
+  //let remove all the images onclick
+  document.getElementById("rock").remove();
+  document.getElementById("paper").remove();
+  document.getElementById("scissor").remove();
+
+  let humanDiv = document.createElement("div");
+  let botDiv = document.createElement("div");
+  let messageDiv = document.createElement("div");
+
+  humanDiv.innerHTML =
+    "<img src ='" +
+    imagesDataBase[humanImageChoice] +
+    "' height=150 width=150 style='box-shadow: 0 10px 50px rgba(0, 0, 0, 0.7)'>";
+
+  messageDiv.innerHTML =
+    "<h1 style= 'color:" +
+    finalMessage["color"] +
+    ";font-size:50px; padding:30px; '>" +
+    finalMessage["message"] +
+    "</h1>";
+
+  botDiv.innerHTML =
+    "<img src ='" +
+    imagesDataBase[botImageChoice] +
+    "' height=150 width=150 style='box-shadow: 0 10px 50px rgba(255, 23, 0, 0.7)'>";
+
+  document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+  document.getElementById("flex-box-rps-div").appendChild(messageDiv);
+  document.getElementById("flex-box-rps-div").appendChild(botDiv);
 }
